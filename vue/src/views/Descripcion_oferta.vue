853cf7f4b4071b1f2c61114376e8c04d4bd53297
MiguelDev2021 <miguelucho.980@hotmail.com>
2021-09-22 17:55:35 -0500

    Se ha creado la interfaz Descripcion_oferta con Vuetify

---
+++ vue/src/views/Descripcion_oferta.vue
@@ -0,0 +1,142 @@
+<template>
+
+  <v-container class="contenedor-all">
+  <v-container id="contenedor_p">
+  <v-carousel :show-arrows="false">
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+    ></v-carousel-item>
+  </v-carousel>
+  </v-container>
+  <v-container class="contenedor-descripcion">  
+  <h1>Nombre del producto</h1>
+
+   <h2>cultivado por :</h2> <v-chip
+      class="ma-2"
+      color="primary"
+      label
+    >
+      <v-icon left>
+        mdi-account-circle-outline
+      </v-icon>
+      John Leider Pedraza
+    </v-chip>
+
+    <h3>Cantidad Disponible:</h3>
+    <p> 4000 Libras </p>
+
+    <h3>Fecha de Entrega:</h3>
+    <p> 10 de Diciembre 2020 </p> 
+
+    <h3> Categoria de cultivo: </h3>
+     <v-chip
+      class="ma-2"
+      color="#64FFDA"
+      label
+    >
+      <v-icon left>
+        mdi-flare
+      </v-icon>
+        Categoria
+    </v-chip>
+
+    <v-divider></v-divider>
+
+      <v-btn
+  elevation="2"
+  x-large
+  color = "green accent-3"
+> ¡RESERVAR YA!</v-btn>
+
+ 
+
+  </v-container>
+    <v-container>
+
+  </v-container>
+  </v-container>
+
+ 
+  
+
+</template>
+
+<script>
+export default {
+  created() {
+    this.$store.commit("SET_LAYOUT", "Interfaz_ventas_categorias");
+  },
+
+  data() {
+    return {
+      items: [
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+
+h2{
+  margin-top: 20px;
+
+}
+
+h3{
+  margin-top: 20px;
+
+}
+
+div.v-window.v-item-group.theme--dark.v-carousel{
+  width: 700px;
+  height: 400px;
+
+
+
+}
+
+
+
+@media (min-width: 960px){
+.container {
+    display: flex;
+}
+}
+
+
+
+
+.contenedor-descripcion{
+
+  display: block  ;
+  margin-top: 20px;
+
+}
+
+.v-application .ma-2{
+  margin: 10px 0px 0px 0px !important;
+
+}
+
+hr{
+
+  margin-top: 30px;
+  margin-bottom:20PX;
+}
+
+</style>
---
+++ vue/src/views/Login.vue
@@ -0,0 +1,103 @@
+<template>
+   <v-container>
+     <p id="texto-iniciar-sesion">INICIAR SECCION</p>
+   <v-form>
+     <v-container>
+      <v-row>
+      <v-col 
+      cols="12"
+        sm="12"
+        lg="12"
+        
+      >
+
+     <v-text-field class="mt-10"
+            label="Correo Electronico"
+          ></v-text-field>
+  
+
+      
+       <v-text-field mt-20
+            label="Contraseña"
+          ></v-text-field>
+      </v-col>
+      </v-row>
+  
+<v-btn
+  depressed
+  elevation="2"
+  raised
+  color = "green accent-3"
+
+> INGRESAR</v-btn>
+
+<v-divider></v-divider>
+  <v-container id="contedor-registrese">
+  <p>¿No tienes cuenta?</p><a href=""> Regitrate</a>
+  </v-container>
+     </v-container>
+   </v-form>
+   </v-container>
+</template>
+
+<script>
+export default {
+  created() {
+    this.$store.commit("SET_LAYOUT", "Interfaz_default");
+  },
+};
+</script>
+
+<style>
+ .container {
+  max-width: 600px;
+  text-align: center;
+  margin-top: 20px ;
+
+}
+
+.container--fluid{
+    border: 1px solid rgb(233, 233, 233);
+    margin-top: 200px;
+    border-radius: 4px ;
+}
+
+div.v-input__control{
+
+  margin-top: 50px;
+}
+
+.v-btn{
+  margin-top: 50px;
+  width: 200px;
+
+
+}
+.v-application .mt-10{
+  margin-top: 10px !important;
+}
+
+.v-btn:not(.v-btn--round).v-size--default{
+  height: 50px;
+}
+
+p{
+  font-size: 15px;
+  display: inline;
+
+}
+.v-application p{
+  margin-bottom: 0px;
+  margin-top: 30px;
+}
+
+hr{
+  margin-top: 40px;
+}
+
+#texto-iniciar-sesion{
+  font-size: 30px;
+  color: #00C853;
+}
+
+</style>
