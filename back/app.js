6e45b1849b29d54b7f159ba4990dbd4165fe367f
MiguelDev2021 <miguelucho.980@hotmail.com>
2021-10-15 16:48:09 -0500

    front add to back

--- back/app.js
+++ back/app.js
@@ -7,19 +7,24 @@ const cors = require("cors")
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //Agregar las ruta a escuchar
 app.use("/api", require("./routes/routes"));
 
 //configuracion con la base de datos
 
-const  moongose  = require("mongoose");
+const moongose = require("mongoose");
 moongose.connect(process.env.BD_URI)
-.then(() => console.log("conectado a la base datos"))
-.catch(err => console.error(err));
+    .then(() => console.log("conectado a la base datos"))
+    .catch(err => console.error(err));
 
-
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(__dirname + "/site/"));
+    app.get("*", (req, res) => {
+        res.sendFile(__dirname + "/site/index.html");
+    });
+};  
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`servidor en http://localhost:${port}`));
